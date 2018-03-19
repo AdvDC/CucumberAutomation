8a98717c56db82a69264856a4c661954ccb4374d
AdvDC <narayan.srinivasan12@gmail.com>
2018-03-19 12:27:25 +1100

    Saved to Dev Branch

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -8,17 +8,17 @@ public class Hooks extends BaseClass {
 	
 	@Before("@smokeTest")
 	public void beforeSmokeTest() {
-		System.out.println("Smoke Test HOOKS - Modified Version");
+		System.out.println("Smoke Test HOOKS - Modified Version from Dev Branch");
 	}
 	
 	@After("@smokeTest")
 	public void afterSmokeTest() {
-		System.out.println("Closing Smoke Test HOOKS - Modified Version");
+		System.out.println("Closing Smoke Test HOOKS - Modified Version Dev Branch");
 //		this.close();
 	}
 	
 	@Before("@sanityTest")
 	public void beforeSanityTest() {
-		System.out.println("Sanity HOOKS - Modified Version");
+		System.out.println("Sanity HOOKS - Modified Version Dev Branch");
 	}
 }
